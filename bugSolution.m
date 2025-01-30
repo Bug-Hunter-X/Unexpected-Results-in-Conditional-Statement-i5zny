599925b68f77c4b24889813e076a09d6e6ae14d8
BugHunter-X <askzenai@gmail.com>
2025-01-30 18:10:05 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,15 @@
+function result = myFunction(x)
+  if x > 10
+    result = x^2; 
+  else
+    result = x^3; 
+  end
+end
+
+% Calling the function with different inputs:
+val1 = myFunction(5);
+val2 = myFunction(15);
+
+% Displaying the results
+disp(val1); % Expected: 125, Actual: 125
+disp(val2); % Expected: 225, Actual: 225
